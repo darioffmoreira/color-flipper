e45a6ddd9ad316f14ff291a8e426fa42c9148749
darioffmoreira <darioffmoreira@gmail.com>
2021-03-20 18:36:59 +0000

    script > added event on click

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
 btn.addEventListener('click', function() {
-    console.log(document.body);
+    // get random number berween 0 - 3
+    const randomNumber = 2;
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+
+    console.log(colors[randomNumber]);
 })
 
 console.log(colors);
